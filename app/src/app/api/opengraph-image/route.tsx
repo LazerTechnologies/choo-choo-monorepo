d53ae9821d1c8c3088b7475bfd6e720eb9a08839
Jon Bray <jonbray@lazertechnologies.com>
2025-09-02 13:36:46 -0400

    🖼️ update OG image to main NFT image

--- app/src/app/api/opengraph-image/route.tsx
+++ app/src/app/api/opengraph-image/route.tsx
@@ -4,15 +4,13 @@ export const dynamic = 'force-dynamic';
 
 export async function GET() {
   const iconUrl =
-    'https://scarlet-quick-grouse-388.mypinata.cloud/ipfs/bafybeidv5hqsasr7asvf7vjrc2jnxh6i3afyvqwxojhjybkbq3g23p75zi';
+    'https://scarlet-quick-grouse-388.mypinata.cloud/ipfs/bafybeibrvjscvkspyexa6lcio2lk72e6l3faqzdgr7pcmrebtwkt75lpbu';
 
   return new ImageResponse(
     (
-      <div tw="flex h-full w-full flex-col justify-center items-center relative bg-purple-600">
+      <div tw="flex h-full w-full relative bg-purple-600">
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={iconUrl} alt="ChooChoo" tw="w-48 h-48 mb-8" />
-        <h1 tw="text-8xl text-white font-bold">ChooChoo</h1>
-        <p tw="text-5xl mt-4 text-white opacity-80">All aboard the train!</p>
+        <img src={iconUrl} alt="ChooChoo" tw="w-full h-full object-cover object-top" />
       </div>
     ),
     {
