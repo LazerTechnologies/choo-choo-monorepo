a4fabc7d0cca6321b8b9424f3b941f54c13b7be6
Jon Bray <jonbray@lazertechnologies.com>
2025-09-02 18:14:33 -0400

    🖼️ updating opengraph image positioning

--- app/src/app/api/opengraph-image/route.tsx
+++ app/src/app/api/opengraph-image/route.tsx
@@ -29,7 +29,7 @@ export async function GET() {
             width: '100%',
             height: '100%',
             objectFit: 'cover',
-            objectPosition: 'top center',
+            objectPosition: 'center top',
           }}
         />
       </div>
